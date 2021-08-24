e50f0232884b23ce85abbfddb8c1e6c24200af34
ToddComte <comtetodd@gmail.com>
2021-08-24 16:21:30 -0600

    continuing testing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
-import React, {Component}from 'react'
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 class Footer extends Component{
-    render(){
-        return(
-            <footer className = 'App-footer'>
-            <h3>I am the footer!</h3>
-            </footer>
-        )
-    }
+  render(){
+    return(
+      <footer>
+        <ul>
+          <NavLink to="/">&copy; 2021 Yan & Todd</NavLink>
+        </ul>
+        <ul>
+          <NavLink to="/catindex">Meet the Cats</NavLink>
+        </ul>
+        <ul>
+          <NavLink to="/catnew">Add a Cat</NavLink>
+        </ul>
+      </footer>
+    )
+  }
 }
-
 export default Footer
