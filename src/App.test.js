b18df24da34c630a513c054bcb753151ab3ee075
ToddComte <comtetodd@gmail.com>
2021-08-24 14:03:55 -0600

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import Enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({adapter: new Adapter()})
+
+describe('when the app renders', () => {
+  it('it describe a header and footer', () => {
+
+  
+  const renderedApp = shallow(<App/>)
+  // act
+  const renderedHeader = renderedApp.find("Header")
+  const renderedFooter = renderedApp.find("Footer")
+  // assert
+  expect(renderedHeader.length).toEqual(1)
+  expect(renderedFooter.length).toEqual(1)
+  })
+  it('provides a route"/" to the home component', () =>{
+  // arrange
+  const renderedApp = shallow(<App />)
+  // act
+  const renderedHomeRoute = renderedApp.find('[path="/"]')
+  console.log("rendered home route debug",
+  renderedHomeRoute.debug())
+  })
+})
+
+
